43597d52a28da9cccc6f4c492c9e1650424e23ee
sun-gwang <kkj80911@gmail.com>
2024-11-13 10:53:31 +0900

    설정파일, 컨트롤러 추가

---
+++ oracleTest/src/main/java/controller/MainController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value= {"/", "/index"})
+	public String index() {
+		
+		return "/index.jsp";
+		
+	}
+}
